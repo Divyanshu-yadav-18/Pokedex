21d4287c4a66dd8d13ba84997056288313b2f13d
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-06-14 00:25:50 +0530

    null check for no further evolution

--- lib/Screens/datail_card.dart
+++ lib/Screens/datail_card.dart
@@ -5,12 +5,18 @@ class DatailCard extends StatelessWidget {
   final Pokemon pokemon;
   const DatailCard({required this.pokemon, super.key});
 
-  bodyCard() => Stack(
+  bodyCard(BuildContext context) => Stack(
     children: <Widget>[
-      Container(
+      Positioned(
+        height: MediaQuery.of(context).size.height / 1.5,
+        width: MediaQuery.of(context).size.width - 20,
+        left: 10.0,
+        top: MediaQuery.of(context).size.height * 0.1,
         child: Card(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
+              SizedBox(height: 50),
               Text(pokemon.name!),
               Text("Height: ${pokemon.height}"),
               Text("Weight: ${pokemon.weight}"),
@@ -20,7 +26,16 @@ class DatailCard extends StatelessWidget {
                 children:
                     pokemon.type!
                         .map(
-                          (t) => FilterChip(label: Text(t), onSelected: (b) {}),
+                          (t) => FilterChip(
+                            backgroundColor: const Color.fromARGB(
+                              255,
+                              184,
+                              237,
+                              228,
+                            ),
+                            label: Text(t),
+                            onSelected: (b) {},
+                          ),
                         )
                         .toList(),
               ),
@@ -30,24 +45,59 @@ class DatailCard extends StatelessWidget {
                 children:
                     pokemon.weaknesses!
                         .map(
-                          (t) => FilterChip(label: Text(t), onSelected: (b) {}),
-                        )
-                        .toList(),
-              ),
-              Text("Next Evolution"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children:
-                    pokemon.nextEvolution!
-                        .map(
-                          (n) => FilterChip(
-                            label: Text(n.name!),
+                          (t) => FilterChip(
+                            backgroundColor: const Color.fromARGB(
+                              255,
+                              192,
+                              140,
+                              241,
+                            ),
+                            label: Text(t),
                             onSelected: (b) {},
                           ),
                         )
                         .toList(),
               ),
+              ...pokemon.nextEvolution == null
+                  ? const [Text("No Further Evolution")]
+                  : [
+                    const Text("Next Evolution"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children:
+                          pokemon.nextEvolution!
+                              .map(
+                                (n) => FilterChip(
+                                  backgroundColor: Color.fromARGB(
+                                    255,
+                                    236,
+                                    170,
+                                    123,
+                                  ),
+                                  label: Text(n.name!),
+                                  onSelected: (b) {},
+                                ),
+                              )
+                              .toList(),
+                    ),
+                  ],
             ],
+          ),
+        ),
+      ),
+      Align(
+        alignment: Alignment.topCenter,
+        child: Hero(
+          tag: pokemon.img!,
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(pokemon.img!),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
         ),
       ),
@@ -58,7 +108,7 @@ class DatailCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(pokemon.name!)),
-      body: bodyCard(),
+      body: bodyCard(context),
     );
   }
 }

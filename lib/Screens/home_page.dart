0288ba1f512db145cfc17fa9004fd6144e74f677
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-06-13 23:03:09 +0530

    fetch data

---
+++ lib/Screens/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:pokedex/data/pokemon_model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  PokeHub? pokeHub;
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var response = await http.get(Uri.parse(url));
+    var decodeJson = jsonDecode(response.body);
+    pokeHub = PokeHub.fromJson(decodeJson);
+
+    setState(() {
+      pokeHub = PokeHub.fromJson(decodeJson);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Poke~Dex")),
+        backgroundColor: Colors.lightGreenAccent,
+      ),
+      body:
+          pokeHub == null || pokeHub!.pokemon == null
+              ? Center(child: CircularProgressIndicator())
+              : GridView.count(
+                crossAxisCount: 2,
+                children: pokeHub!.pokemon!.map((pok) => Card()).toList(),
+              ),
+
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightGreenAccent,
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
